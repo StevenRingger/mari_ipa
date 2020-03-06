4057dd0c0f04825a49e108c6bde81b1fb8fb6c7d
Steven Ringger <steven.ringger@hotmail.ch>
2020-03-06 11:07:23 +0100

    initial Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import NavbarTest from './components/organisms/navbar/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <NavbarTest />
+    </div>
+  );
+}
+
+export default App;
